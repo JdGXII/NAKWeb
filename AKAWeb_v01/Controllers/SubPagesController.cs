7ddaba6bce5dfda9c7f391e8c77d54ae5c765002
JdGXII <jralzaibar@yahoo.com>
2017-05-12 19:49:40 -0400

    Added links to Kin Today and workshop in lower row images. Took out headers and added padding

---
+++ AKAWeb_v01/Controllers/SubPagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AKAWeb_v01.Controllers
+{
+    public class SubPagesController : Controller
+    {
+        // GET: SubPages
+        public ActionResult KinToday()
+        {
+            return View("~/Views/SubPages/Publications/KinesiologyToday.cshtml");
+        }
+
+        public ActionResult Workshop()
+        {
+            return View("~/Views/SubPages/EventsServices/WorkshopRegistration.cshtml");
+        }
+    }
+}
