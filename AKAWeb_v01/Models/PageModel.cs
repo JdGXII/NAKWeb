f245e5b3da8d00e2660cb32d88f94edef0e36469
JdGXII <jralzaibar@yahoo.com>
2017-06-13 11:53:51 -0400

    Created View model to populate subpages.

---
+++ AKAWeb_v01/Models/PageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AKAWeb_v01.Models
+{
+    public class PageModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string subheaderImage { get; set; }
+        public List<MainMenuItem> leftMenu { get; set; }
+        public string pageContent { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime modifiedAt { get; set; }
+
+    }
+}
