233648b595832130c529f92c93bdfca338b53e06
JdGXII <jralzaibar@yahoo.com>
2017-07-06 15:51:18 -0400

    Modified SectionModel. Added new property; a list that represents the pages that belong to the section. Overloaded the constructor to reflect this.

--- AKAWeb_v01/Models/SectionModel.cs
+++ AKAWeb_v01/Models/SectionModel.cs
@@ -5,17 +5,30 @@ using System.Web;
 
 namespace AKAWeb_v01.Models
 {
+    //This class not only serves to model a section, said modelling helps the creation of the top menu
+    //The top menu is the menu below the header on the main site. It is created dynamically by loading sections
+    //Then listing the pages beneath the section
     public class SectionModel
     {
         public int id { get; set; }
         public string title { get; set; }
         public bool isLive { get; set; }
+        //optional. A list with the pages that belong to the section
+        public List<PageModel> pages { get; set; }
 
         public SectionModel(int id, string title, bool isLive)
         {
             this.id = id;
             this.title = title;
             this.isLive = isLive;
+        }
+
+        public SectionModel(int id, string title, bool isLive, List<PageModel> pages)
+        {
+            this.id = id;
+            this.title = title;
+            this.isLive = isLive;
+            this.pages = pages;
         }
     }
 }
