985e0fcbd84ca884fe946add993e0f63e816b727
JdGXII <jralzaibar@yahoo.com>
2017-07-06 16:18:38 -0400

    Added functions to controller menu controller to get all sections with associated pages and pass it to the view as the model.

--- AKAWeb_v01/Controllers/MenuController.cs
+++ AKAWeb_v01/Controllers/MenuController.cs
@@ -17,10 +17,34 @@ namespace AKAWeb_v01.Controllers
             return View();
         }
 
+        private List<AKAWeb_v01.Models.PageModel> getPages(int section_id)
+        {
+            List<AKAWeb_v01.Models.PageModel> page_list = new List<PageModel>();
+            DBConnection testconn = new DBConnection();
+            string query = "SELECT id, title, subheader_image, content, section, isAlive from Pages WHERE isAlive = 1 AND section = "+section_id.ToString();
+            SqlDataReader dataReader = testconn.ReadFromTest(query);
+            //while there are records in the datareader
+            while (dataReader.Read())
+            {
+                int id = Int32.Parse(dataReader.GetValue(0).ToString());
+                string title = dataReader.GetValue(1).ToString();
+                string subheaderImage = dataReader.GetValue(2).ToString();
+                string pageContent = dataReader.GetValue(3).ToString();
+                int section = Int32.Parse(dataReader.GetValue(4).ToString());
+                bool isAlive = (bool)dataReader.GetValue(5);
+                PageModel page = new PageModel(id, title, subheaderImage, pageContent, section, isAlive);
+                page_list.Add(page);
+
+            }
+
+            return page_list;
+
+        }
+
         private List<SectionModel> getSections()
         {
             DBConnection testconn = new DBConnection();
-            string query = "SELECT * from Sections";
+            string query = "SELECT * from Sections WHERE isAlive = 1";
             SqlDataReader dataReader = testconn.ReadFromTest(query);
             List<SectionModel> sections = new List<SectionModel>();
             while (dataReader.Read())
@@ -28,12 +52,21 @@ namespace AKAWeb_v01.Controllers
                 int id = Int32.Parse(dataReader.GetValue(0).ToString());
                 string title = dataReader.GetValue(1).ToString();
                 bool isLive = (bool)dataReader.GetValue(2);
-                SectionModel section = new SectionModel(id, title, isLive);
+                List<PageModel> pages = getPages(id);
+                SectionModel section = new SectionModel(id, title, isLive, pages);
                 sections.Add(section);
 
             }
 
             return sections;
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult MainMenu()
+        {
+            var model = getSections();
+
+            return PartialView("_MainMenu", model);
         }
     }
 }
