e9dab1d4b3f7c2c8d31062e1581b911c030a43c6
JdGXII <jralzaibar@yahoo.com>
2017-07-10 17:46:12 -0400

    Added Product base model and Membership derived model.

---
+++ AKAWeb_v01/Models/MembershipModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AKAWeb_v01.Models
+{
+    public class MembershipModel : ProductModel
+    {
+        public string type { get; set; }
+        public string description { get; set; }
+
+        public MembershipModel(int id, int cost, string type, string description) : base (id, cost)
+        {
+            this.type = type;
+            this.description = description;
+
+        }
+    }
+}
---
+++ AKAWeb_v01/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AKAWeb_v01.Models
+{
+    public abstract class ProductModel
+    {
+        public int id { get; set; }
+        public int cost { get; set; }
+        
+        public ProductModel(int id, int cost)
+        {
+            this.id = id;
+            this.cost = cost;
+        }
+    }
+}
